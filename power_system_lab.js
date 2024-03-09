a84224a4ea8281ad79610649dc5f9ce99cb48a27
sachinargadejspm <162416994+sachinargadejspm@users.noreply.github.com>
2024-03-09 13:17:41 +0530

    Add files via upload

---
+++ power_system_lab.js
@@ -0,0 +1,24 @@
+var currentImg = 0;
+var imgs = document.querySelectorAll('.slider img');
+let dots = document.querySelectorAll('.dot');
+var interval = 2800;
+
+var timer = setInterval(changeSlide, interval);
+
+function changeSlide(n) {
+  for (var i = 0; i < imgs.length; i++) {
+    imgs[i].style.opacity = 0;
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+
+  currentImg = (currentImg + 1) % imgs.length;
+
+  if (n != undefined) {
+    clearInterval(timer);
+    timer = setInterval(changeSlide, interval);
+    currentImg = n;
+  }
+
+  imgs[currentImg].style.opacity = 1;
+  dots[currentImg].className += ' active';
+}
